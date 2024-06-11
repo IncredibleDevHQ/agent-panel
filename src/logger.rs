efa5305534b4acd225e5351da1ca14dea553fe56
Karthic Rao <kartronics85@gmail.com>
2024-06-10 21:22:50 -0700

    Refactor logger setup in setup_logger function

--- src/logger.rs
+++ src/logger.rs
@@ -7,12 +7,7 @@ use simplelog::{format_description, Config as LogConfig, ConfigBuilder};
 #[cfg(debug_assertions)]
 pub fn setup_logger(working_mode: WorkingMode) -> Result<()> {
     let config = build_config();
-    if working_mode == WorkingMode::Serve {
         simplelog::SimpleLogger::init(LevelFilter::Debug, config)?;
-    } else {
-        let file = std::fs::File::create(crate::config::Config::local_path("debug.log")?)?;
-        simplelog::WriteLogger::init(LevelFilter::Debug, config, file)?;
-    }
     Ok(())
 }
 
@@ -26,12 +21,7 @@ pub fn setup_logger(working_mode: WorkingMode) -> Result<()> {
 }
 
 fn build_config() -> LogConfig {
-    let log_filter = match std::env::var("AICHAT_LOG_FILTER") {
-        Ok(v) => v,
-        Err(_) => "aichat".into(),
-    };
     ConfigBuilder::new()
-        .add_filter_allow(log_filter)
         .set_time_format_custom(format_description!(
             "[year]-[month]-[day]T[hour]:[minute]:[second].[subsecond digits:3]Z"
         ))
